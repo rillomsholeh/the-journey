f59d4b76fafb7dd8c99726cf9686f2d735ad651a
aseppp <saepudinasep86@gmail.com>
2022-03-25 14:20:59 +0700

    checkpoint

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,8 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
 // CREATE BASE URL
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL:
+    process.env.REACT_APP_SERVER_URL ||
+    "https://app-journey.herokuapp.com/api/v1" ||
+    "https://localhost:5000/api/v1",
 });
 
 export const setAuthToken = (token) => {
